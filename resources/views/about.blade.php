5c9dc7034e472d74723c8509e8c03a6e344cf1bb
nikznk <nikz.kamble@hotmail.com>
2019-07-09 03:07:00 +1200

    started using @yield

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+
+@section('title','About Page')
+
+@section('content')
+<h1>abiout</h1>
+
+<p>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis, porro eveniet quo quisquam voluptates labore voluptatibus voluptas, omnis qui eos nam. Consequuntur, debitis doloribus. Odio quidem repellendus sit sint laborum.</p>
+
+@endsection
